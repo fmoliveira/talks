a193eb66b977a6fcd6b7019ac5681dfdfed723f4
Filipe Oliveira <contato@fmoliveira.com.br>
2020-09-18 01:06:38 -0300

    Setup next-on-netlify.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withMDX = require('@next/mdx')({
   extension: /\.(md|mdx)$/,
 })
 module.exports = withMDX({
+  target: 'serverless',
   // Pick up MDX files in the /pages/ directory
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
 })
